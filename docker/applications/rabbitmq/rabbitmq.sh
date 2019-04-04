56b360e1551bcc6943b243132279382f3141f6db
surick <jk103@qq.com>
2019-04-04 16:15:18 +0800

    fix rabbitmq

--- docker/applications/rabbitmq/rabbitmq.sh
+++ docker/applications/rabbitmq/rabbitmq.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker pull rabbitmq:3-management
-# docker run -d --hostname rabbitmq --name rabbitmq rabbitmq:3
-docker run -d --hostname rabbitmq --name rabbitmq-management -e RABBITMQ_DEFAULT_USER=root -e RABBITMQ_DEFAULT_PASS=evilcry rabbitmq:3-management
+#docker run -p 5671:5672 -d --hostname rabbitmq --name rabbitmq rabbitmq:3
+docker run -d -p 15671:15672 -p 5671:5672 --hostname rabbitmq --name rabbitmq-management -e RABBITMQ_DEFAULT_USER=root -e RABBITMQ_DEFAULT_PASS=evilcry rabbitmq:3-management
 docker ps
