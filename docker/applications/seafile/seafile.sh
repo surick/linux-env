2cc756bab4f4db0afba3579716f74713390130be
surick <jk103@qq.com>
2019-04-04 18:14:44 +0800

    add ide env

--- docker/applications/seafile/seafile.sh
+++ docker/applications/seafile/seafile.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 # a cloud storage
-mkdir -p /root/docker/volumes
+mkdir -p /root/docker/volumes/seafile
 docker pull seafileltd/seafile:latest
-docker run -d --name seafile
--e SEAFILE_SERVER_HOSTNAME=seafile.example.com
--e SEAFILE_ADMIN_EMAIL=me@example.com
--e SEAFILE_ADMIN_PASSWORD=a_very_secret_password
--v $HOME/docker/volumes/seafile:/shared
--p 80:80 seafileltd/seafile:latest
+docker run -d --name seafile -e SEAFILE_SERVER_HOSTNAME=seafile.example.com -e SEAFILE_ADMIN_EMAIL=me@example.com -e SEAFILE_ADMIN_PASSWORD=a_very_secret_password -v $HOME/docker/volumes/seafile:/shared -p 80:80 seafileltd/seafile:latest
