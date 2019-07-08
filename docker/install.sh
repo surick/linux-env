9b164f5b24d92c3b22caccfb5b271d44e7e5f9e9
Evil Cry <jk103@qq.com>
2019-07-08 15:53:50 +0800

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+sudo yum -y install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
